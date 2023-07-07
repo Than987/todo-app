7034f3d22c3d80329c4c2cf7eb20a294b323fe55
Than singh (Rishi) <68466601+Than987@users.noreply.github.com>
2023-07-07 10:25:28 +0530

    Update TodoWindow.js

--- todo-app/src/components/TodoWindow.js
+++ todo-app/src/components/TodoWindow.js
@@ -52,7 +52,7 @@ export const TodoWindow = () => {
                     </div>
                 </div>
             </div>
-
+<hr></hr>
             <table class="table">
                 <thead>
                     <tr>
