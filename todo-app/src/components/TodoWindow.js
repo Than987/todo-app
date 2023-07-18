835a88301ae68d17811db376e0773163791d3280
Than singh <rishidogra987@gmail.com>
2023-07-19 00:28:15 +0530

    Added: 1.Modal when user hits the Edit Note, 2.A single Note Deletion Functionality, 3.Clear complete writing Note/text Functionality

--- todo-app/src/components/TodoWindow.js
+++ todo-app/src/components/TodoWindow.js
@@ -5,33 +5,65 @@ import '../App.css';
 export const TodoWindow = () => {
     const [todotask, setTodotask] = useState("");
     const [todadata, setTododata] = useState([]);
+    const [modal, setModal] = useState(false);
 
     useEffect(() => {
         GetNote();
-    },[])
+    }, [todadata])
 
     const handleChange = (e) => {
         e.preventDefault();
         const name = e.target.name;
-        const value= e.target.value;
-        setTodotask({...todotask,[name]:value})
+        const value = e.target.value;
+        setTodotask({ ...todotask, [name]: value })
     }
 
     const PostNote = () => {
         axios.post(`https://6303ab6a0de3cd918b3bbabb.mockapi.io/crud-data`, todotask)
             .then(res => res.data)
         setTodotask("")
-    
+
     }
 
     const GetNote = () => {
-        axios.get(`https://6303ab6a0de3cd918b3bbabb.mockapi.io/crud-data}`)
+        axios.get(`https://6303ab6a0de3cd918b3bbabb.mockapi.io/crud-data`)
             .then(res =>
-                 setTododata((res.data)))
+                setTododata((res.data)))
     }
+
+    const DeleteNote = (id) => {
+        axios.delete(`https://6303ab6a0de3cd918b3bbabb.mockapi.io/crud-data/${id}`)
+            .then((res) => {
+                console.log(res.data)
+            })
+    }
+
+
+    const ModalWindow = () => {
+        return (
+            <>
+                <input type='text' placeholder='Update Note' name='Task' />
+                <button onClick={UpdateModal}>Update</button>
+            </>
+        )
+    }
+
+    const UpdateModal =() => {
+        setModal(false)
+    }
+
+    const OpenModal = () => {
+        setModal(true)
+    }
+
+    const ClearTextField = () =>{
+        setTodotask("");
+    }
+
 
     return (
         <>
+            {modal && <ModalWindow />}
             <div className='modall'>
                 <div className="modal-fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                     <div className="modal-dialog">
@@ -45,14 +77,14 @@ export const TodoWindow = () => {
                                 </div>
                             </div>
                             <div className="modal-footer">
-                                <button type="button" className="btn btn-danger" data-bs-dismiss="modal">Clear Note</button>
+                                <button type="button" className="btn btn-danger" data-bs-dismiss="modal" onClick={ClearTextField}>Clear Note</button>
                                 <button type="button" onClick={PostNote} className="btn btn-success">Save Note</button>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-<hr></hr>
+            <hr></hr>
             <table class="table">
                 <thead>
                     <tr>
@@ -61,14 +93,16 @@ export const TodoWindow = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    {todadata && todadata.map((note) =>{
-                        return(
-                        <>
-                            <tr>
-                                <td>{note.id}</td>
-                                <td>{note.Task}</td>
-                            </tr>
-                        </>
+                    {todadata && todadata.map((note) => {
+                        return (
+                            <>
+                                <tr>
+                                    <td>{note.id}</td>
+                                    <td>{note.Task}</td>
+                                    <td ><button onClick={() => OpenModal(note.id)}>Edit Note</button></td>
+                                    <td ><button onClick={() => DeleteNote(note.id)}>Delete Note</button></td>
+                                </tr>
+                            </>
                         )
                     })}
                 </tbody>
